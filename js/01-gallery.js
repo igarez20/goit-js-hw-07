5be29ef6c8624014747b993b1fb58917a5b5af35
Ihor Rusyn <igarez20@gmail.com>
2023-03-25 15:44:26 +0200

    js for gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,3 +4,24 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryContainer = document.querySelector(".gallery");
+const galleryCardSet = createGalleryItemsMarkUp(galleryItems);
+
+function createGalleryItemsMarkUp(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardSet);
